7b53cb37297cb0970f56d867d7c935ff931299ae
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-01-03 13:36:03 -0700

    import Bootstrap to project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// bootstrap
+import { Button } from 'react-bootstrap'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +20,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Button variant='dark'>This is a bootstrap button</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
