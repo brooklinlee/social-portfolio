cf328ca242716ec42edccdc1c7c9fd62cb1e9d7a
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-01-05 12:13:33 -0700

    stub up home page and nav bar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 // bootstrap
-import { Button } from 'react-bootstrap'
+
+
+// pages
+import Home from './pages/Home'
+
+// components
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
-      <h1 className="text-3xl font-bold ">Vite + React</h1>
-      <Button variant='dark'>This is a bootstrap button</Button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <NavBar />
+    <main>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </main>
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom"
+
+// images
+import SMMLogo from '../assets/images/SMM-Logo.svg'
+
+const NavBar = () => {
+  return ( 
+    <header>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <NavLink to='/' >
+            <img src={SMMLogo} alt="brooklin Logo" />
+          </NavLink>
+          <NavLink to='/' >HOME</NavLink>
+
+        </div>
+      </nav>
+    </header>
+
+  )
+}
+
+export default NavBar
