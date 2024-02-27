9dc61a31a68e220a3543598773bd7eb2a2a57c8b
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-27 15:35:11 -0700

    stub up HomeHeader component and place in Home page

---
+++ src/components/Home/HomeHeader.jsx
@@ -0,0 +1,12 @@
+// css
+import './HomeHeader.css'
+
+const HomeHeader = () => {
+  return (  
+    <>
+      <h1>Home Header</h1>
+    </>
+  )
+}
+
+export default HomeHeader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import HomeHeader from "../components/Home/HomeHeader"
+
 const Home = () => {
   return (  
     <>
     <h1>This is the home page</h1>
     <p>P Tag Test</p>
+    <HomeHeader />
     </>
   )
 }
