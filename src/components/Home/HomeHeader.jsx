c535303ab29224a4fc57137ba861a8ab35c0ecef
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-27 19:04:40 -0700

    stub up html content on HomeHeader component

--- src/components/Home/HomeHeader.jsx
+++ src/components/Home/HomeHeader.jsx
@@ -5,7 +5,22 @@ const HomeHeader = () => {
   return (  
     <>
     <main className='full-screen-view'>
-      <h1>Home Header</h1>
+      <div className='left-div'>
+        <h1>Hi. I'm Brooklin<br></br>I do Social Media</h1>
+        <p>Stop struggling with managing your social media presence alone. <br></br>Instead, work with a professional to optimize your social media strategy for impactful results.</p>
+        <div>
+          <ul>
+            <li>Expertise in social media trends</li>
+            <li>Time-saving content creation</li>
+            <li>Enhanced audience engagement strategies</li>
+            <li>Consistent brand representation online</li>
+          </ul>
+        </div>
+      </div>
+      <div className='right-div'>
+        <p>Image</p>
+        <div className="blue-shape"></div>
+      </div>
     </main>
     </>
   )
