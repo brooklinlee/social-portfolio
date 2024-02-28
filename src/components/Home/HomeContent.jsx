cc31c8752dd32383a315a3663c60ddbc0760c8ff
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-28 16:05:44 -0700

    add Phones.svg to HomeContent component

--- src/components/Home/HomeContent.jsx
+++ src/components/Home/HomeContent.jsx
@@ -1,12 +1,15 @@
 // css
 import './HomeContent.css'
 
+// assets
+import Phones from '../../assets/images/Phones.svg'
+
 const HomeContent = () => {
   return (  
     <>
-      <main className='full-screen-view'>
+      <main className='home-content'>
         <h1>Your Account Could Look Like This</h1>
-
+        <img src={Phones} alt="" />
       </main>
     </>
   )
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,6 +1,6 @@
 const addScrollListener= (navbar) => {
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 40) {
       navbar.classList.add('scrolled')
     } else {
       navbar.classList.remove('scrolled')
