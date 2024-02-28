c8029e067330c6226d7ddf2fcca6d049e9e4a4ae
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-28 16:34:01 -0700

    add second half of half circle in HomePurpose component

--- src/components/Home/HomePurpose.jsx
+++ src/components/Home/HomePurpose.jsx
@@ -1,11 +1,12 @@
 // css
+import './HomePurpose.css'
 
 const HomePurpose = () => {
   return (  
     <>
       <main className="home-purpose">
-        <h1 className="heading-secondary">Content with a Purpose</h1>
         <div className="half-circle-two"></div>
+        <h1 className="heading-secondary">Content with a Purpose</h1>
       </main>
     </>
   )
