e9c6227fee1ad196fa74c2b3f7ae9f37b3c14a12
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-28 16:50:04 -0700

    add font awesome icons to HomePurpose component

--- src/components/Home/HomePurpose.jsx
+++ src/components/Home/HomePurpose.jsx
@@ -1,12 +1,19 @@
 // css
 import './HomePurpose.css'
 
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faPeopleLine, faArrowUpRightDots } from '@fortawesome/free-solid-svg-icons'
+
 const HomePurpose = () => {
   return (  
     <>
       <main className="home-purpose">
         <div className="half-circle-two"></div>
         <h1 className="heading-secondary">Content with a Purpose</h1>
+        <div><span><FontAwesomeIcon icon={faPeopleLine}/></span></div>
+        <div><span><FontAwesomeIcon icon={faHeart}/></span></div>
+        <div><span><FontAwesomeIcon icon={faArrowUpRightDots}/></span></div>
       </main>
     </>
   )
