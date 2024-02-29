b1b41d96792ffe51a885401ee7119c88e3fb5b8d
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-02-28 20:20:15 -0700

    update multiple icon properties such as spacing, font, and more

--- src/components/Home/HomePurpose.jsx
+++ src/components/Home/HomePurpose.jsx
@@ -12,20 +12,20 @@ const HomePurpose = () => {
         <div className="half-circle-two"></div>
         <h1 className="heading-secondary">Content with a Purpose</h1>
           <div className='icon-section'>
-            <div>
+            <div className='icon'>
               <div className='icon-circle'><span><FontAwesomeIcon icon={faPeopleLine}/></span></div>
               <h2>Community Building</h2>
               <p>Building a vibrant online community fosters deeper connections with your audience, fostering loyalty and driving long-term engagement.</p>
             </div>
-            <div>
+            <div className='icon'>
             <div className='icon-circle'><span><FontAwesomeIcon icon={faHeart}/></span></div>
               <h2>Captivating Audiences</h2>
-              <p>Strategically crafted SEO-Driven content to captivate your audience's attention from the first click, ensuring your message reaches its intended audience effectively</p>
+              <p>Strategically crafted SEO-Driven content to captivate your audience's attention from the first click, ensuring your message reaches its intended audience effectively.</p>
             </div>
-            <div>
+            <div className='icon'>
               <div className='icon-circle'><span><FontAwesomeIcon icon={faArrowUpRightDots}/></span></div>
               <h2>Elevating Standards</h2>
-              <p>Quality content creation establishes your brand as an authority in your niche, driving credibility, trust, and ultimately, conversions</p>
+              <p>Quality content creation establishes your brand as an authority in your niche, driving credibility, trust, and ultimately, conversions.</p>
             </div>
         </div>
       </main>
