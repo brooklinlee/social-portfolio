d2e8e47c8f37f6e85e9bfbc8c532bd0e85438817
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-03-12 09:40:01 -0600

    add base styling to Content Card component

--- src/components/Portfolio/ContentCard.jsx
+++ src/components/Portfolio/ContentCard.jsx
@@ -1,11 +1,13 @@
+import './ContentCard.css'
+
 const ContentCard = ({ content, caption, purpose, equipment }) => {
   return (  
     <div className="content-card">
       <div className="content">{ content }</div>
       <div className="info">
-        <p>{ caption }</p>
-        <p>{ purpose }</p>
-        <p>{ equipment }</p>
+        <p><em>{ caption }</em></p>
+        <p><strong>Purpose:</strong> { purpose }</p>
+        <p><strong>Equipment Used:</strong> { equipment }</p>
       </div>
     </div>
   )
