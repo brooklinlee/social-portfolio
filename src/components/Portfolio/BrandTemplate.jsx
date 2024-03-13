97e3934e2ccf6725a126317a2a01cba4e00e8220
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2024-03-12 21:10:26 -0600

    minor formatting corrections and adjustment of content cards css

--- src/components/Portfolio/BrandTemplate.jsx
+++ src/components/Portfolio/BrandTemplate.jsx
@@ -29,58 +29,55 @@ const BrandTemplate = ({ brand }) => {
         <p>{brand.pDescriptorTwo}</p>
       </section>
       <section className='bt-content'>
-  {brand.videos && brand.videos.length > 0 && (
-    <div className="content-section">
-      <h3 className='heading-primary'>Reels & Videos</h3>
-      <div className="video">
-        {brand.videos.map((video, index) => (
-          <ContentCard
-            key={index}
-            content={<video src={video.src} controls preload='auto' />}
-            caption={video.caption}
-            purpose={video.purpose}
-            equipment={video.equipment}
-          />
-        ))}
-      </div>
-    </div>
-  )}
-
-  {brand.images && brand.images.length > 0 && (
-    <div className="content-section">
-      <h3 className='heading-primary'>Images & Graphics</h3>
-      <div className="images">
-        {brand.images.map((image, index) => (
-          <ContentCard
-            key={index}
-            content={<img src={image.src} alt={`Image ${index + 1}`} />}
-            caption={image.caption}
-            purpose={image.purpose}
-            equipment={image.equipment}
-          />
-        ))}
-      </div>
-    </div>
-  )}
-
-  {brand.carousel && brand.carousel.length > 0 && (
-    <div className="content-section">
-      <h3 className='heading-primary'>Carousel Posts</h3>
-      <div className="carousel-section">
-        {brand.carousel.map((carousel, index) => (
-          <ContentCard
-            key={index}
-            content={<Carousel images={carousel.images} />}
-            caption={carousel.caption}
-            purpose={carousel.purpose}
-            equipment={carousel.equipment}
-          />
-        ))}
-      </div>
-    </div>
-  )}
-</section>
-
+        {brand.videos && brand.videos.length > 0 && (
+          <div className="content-section">
+            <h3 className='heading-primary'>Reels & Videos</h3>
+            <div className="video">
+              {brand.videos.map((video, index) => (
+                <ContentCard
+                  key={index}
+                  content={<video src={video.src} controls preload='auto' />}
+                  caption={video.caption}
+                  purpose={video.purpose}
+                  equipment={video.equipment}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {brand.images && brand.images.length > 0 && (
+          <div className="content-section">
+            <h3 className='heading-primary'>Images & Graphics</h3>
+            <div className="images">
+              {brand.images.map((image, index) => (
+                <ContentCard
+                  key={index}
+                  content={<img src={image.src} alt={`Image ${index + 1}`} />}
+                  caption={image.caption}
+                  purpose={image.purpose}
+                  equipment={image.equipment}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {brand.carousel && brand.carousel.length > 0 && (
+          <div className="content-section">
+            <h3 className='heading-primary'>Carousel Posts</h3>
+            <div className="carousel-section">
+              {brand.carousel.map((carousel, index) => (
+                <ContentCard
+                  key={index}
+                  content={<Carousel images={carousel.images} />}
+                  caption={carousel.caption}
+                  purpose={carousel.purpose}
+                  equipment={carousel.equipment}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </section>
     </main>
   )
 }
